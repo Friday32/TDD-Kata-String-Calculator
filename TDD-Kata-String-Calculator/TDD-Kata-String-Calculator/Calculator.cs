5452be8297e14a5395361c032a50e90a7d6e0bc8
=Jim Petak <jpetak@keithley.com>
2019-08-02 23:04:41 -0400

    Adding project.

---
+++ TDD-Kata-String-Calculator/TDD-Kata-String-Calculator/Calculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDD_Kata_String_Calculator
+{
+    public class Calculator
+    {
+
+    }
+}
---
+++ TDD-Kata-String-Calculator/TDD-Kata-String-Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDD_Kata_String_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
